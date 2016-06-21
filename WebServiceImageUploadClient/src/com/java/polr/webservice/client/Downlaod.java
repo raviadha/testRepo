a8203a135a9ce7fafbae5d43bbbd49378b5ca85a
Vishu <Vishu@Vishu-PC>
2016-06-21 23:21:00 +0530

    Web Service for Uploading image

---
+++ WebServiceImageUploadClient/src/com/java/polr/webservice/client/Downlaod.java
@@ -0,0 +1,32 @@
+
+package com.java.polr.webservice.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for downlaod complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="downlaod">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "downlaod")
+public class Downlaod {
+
+
+}
---
+++ WebServiceImageUploadClient/src/com/java/polr/webservice/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.polr.java.com/")
+package com.java.polr.webservice.client;
